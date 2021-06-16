24c709f9941c25811fccf4aad4639567b6baaa01
dglabs-psg <69159534+dglabs-psg@users.noreply.github.com>
2021-06-16 12:20:41 +0100

    Add files via upload

--- Modules/Deploy_DGAutomation.ps1
+++ Modules/Deploy_DGAutomation.ps1
@@ -61,47 +61,72 @@ if($ExitCode -eq 0){Write-Host "Success: Code=$ExitCode" -for green}else{Write-H
 Write-Host "Deploying AutoIt Library 1.1. from Google..." -for cyan
 Invoke-WebRequest -Uri "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/robotframework-autoitlibrary/AutoItLibrary-1.1.zip" -OutFile "C:\deploy\AutoItLibrary-1.1.zip"
 Expand-Archive -LiteralPath "C:\deploy\AutoItLibrary-1.1.zip" -DestinationPath "$Env:SYSTEMDRIVE\"
+#Setup AutoIt Library 1.1
 &cmd.exe /c "cd $Env:SYSTEMDRIVE\AutoItLibrary-1.1\ & c:\python27\python.exe `"$Env:SYSTEMDRIVE\AutoItLibrary-1.1\setup.py`" install"
 Write-Host "Success: Code=0" -for green
 
 #ROBOT FRAMEWORK
 ##install robotframework & selenium library
 Write-Host "Deploying robotframework & selenium library..." -for cyan
-#&pip install wheel
-#&pip install wxPython==4.1.0
-#&pip install wxPython
-#&pip install robotframework==3.0.4
-#&pip install robotframework-archivelibrary==0.4.0
-#&pip install robotframework-autoitlibrary
-#&pip install robotframework-autoitlibrary==1.1.0
-#&pip install robotframework-autoitlibrary==1.2.5
-#&pip install robotframework-ride  #ride is not needed
-#&pip install robotframework-seleniumlibrary 
-#&pip install robotframework-selenium2library 
-#&pip install decorator
-#&pip install robotframework-selenium2library==1.8.0
+pip install robotframework-remoterunner
+pip install robotframework==3.0.4
+pip install wheel
+pip install wxPython==4.1.0
+pip install robotframework==3.0.4
+pip install robotframework-archivelibrary==0.4.0
+pip install robotframework-autoitlibrary==1.1.0
+pip install decorator
+pip install robotframework-selenium2library==1.8.0
+pip install pyodbc
+pip install jdcal
+pip install et-xmlfile
+pip install openpyxl
+pip install xlwt
+pip install xlrd
+pip install xlutils
+pip install checksumdir
+pip install xlwings
+pip install psutil
 
 
-#install supporting libraries
-Write-Host "Deploying remaining supporting libraries..." -for cyan
-#&pip install pika (do manually exact version, install is incompatbile version)
-#&pip install pyodbc
-#&pip install jdcal
-#&pip install et-xmlfile
-#&pip install openpyxl
-#&pip install xlwt
-#&pip install xlrd
-#&pip install xlutils
-#&pip install checksumdir
-#&pip install xlwings
-#&pip install piexif (do manually exact version, install is incompatbile version)
-#&pip install pymysql
-#&pip install scandir
+#FETCH AUTOMATION_PSG LIBRARY
+Write-Host "Deploying AUTOMATION_PSG LIBRARY (requires manual login to GIT)..." -for cyan
+Set-Service ssh-agent -StartupType Manual
+ssh-agent -s
+$cmdmsg = '!!MANUAL INTERACTION NECESSARY!!'
+$cmdtorun = 'ssh-keygen -b 4096 -t rsa -f id_rsa -q -N ""Your_passphrase_here""'
+$shell = new-object -comobject "WScript.Shell"
+$shell.popup("Run this command in another Administrator PS window and then click OK to continue on.`r`n`r`n$cmdtorun",0,$cmdmsg,0)
+$keyPath = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath('~/.ssh')
+ssh-agent -s
+ssh-add "$keyPath\id_rsa"
+$cmdmsg = '!!NEW SSH KEYPAIR CREATED!!'
+$cmdtorun = Get-Content "$keyPath\id_rsa.pub"
+Start-Process notepad.exe "$keyPath\id_rsa.pub" -WindowStyle Minimized
+$shell.popup("Copy this public key into GITHUB as a new deploy token for the robotfw repo. and then click OK to continue on. Notepad will have opened (minimised) just copy/paste. `r`n`r`n NOTE: Just read only write is not necessary and a risk.`r`n`r`n$cmdtorun",0,$cmdmsg,0)
+mkdir c:/Automation_PSG
+cd c:/Automation_PSG
+git init
+
+#TRYING TO GET AROUND 1GB RAM issue with GIT. GIT needs more than 1G
+#set GIT_TRACE_PACKET=1
+#set GIT_TRACE=1
+#set GIT_CURL_VERBOSE=1
+#git config pack.packSizeLimit 1g
+#git config pack.deltaCacheSize 1g
+#git config pack.windowMemory 1g
+#git config core.packedGitLimit 1g
+#git config core.packedGitWindowSize 1g
+
+git pull git@github.com:dglabs-psg/robotfw.git
 
 #Executy PY setup
-&c:\python27\python.exe c:\Automation\SetupEnv.py
+&c:\python27\python.exe c:\Automation_PSG\SetupEnv.py
 
-#set defender back on
+#add exclusions and set defender back on
+Add-MpPreference -ExclusionPath "C:\Automation_PSG";
+Add-MpPreference -ExclusionPath "C:\AutoItLibrary-1.1";
+Add-MpPreference -ExclusionPath "C:\tools";
 Set-MpPreference -DisableRealtimeMonitoring $false
 Set-MpPreference -DisableIOAVProtection $false
 
